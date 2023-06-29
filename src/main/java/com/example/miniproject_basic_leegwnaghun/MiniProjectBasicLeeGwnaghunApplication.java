be6f1c0cbb8d706e3baf0d10a32e6d005b3c2410
hun053@naver.com <hun053@naver.com>
2023-06-29 13:57:20 +0900

    feat: 초기 설정

---
+++ src/main/java/com/example/miniproject_basic_leegwnaghun/MiniProjectBasicLeeGwnaghunApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniproject_basic_leegwnaghun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectBasicLeeGwnaghunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectBasicLeeGwnaghunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniproject_basic_leegwnaghun/MiniProjectBasicLeeGwnaghunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproject_basic_leegwnaghun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjectBasicLeeGwnaghunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
